06a0b06f589a3a0c4f054ff6cfd0acde52a96fe8
LoganL <ll8875@g.rit.edu>
2019-03-22 10:52:38 -0400

    Addded basic Game Manager script

---
+++ Assets/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    int coins;      //Number of coins
+    int numberFish; //Number of owned fish
+
+    public int Coins
+    {
+        get { return coins; }
+        set
+        {
+            if(value >= 0)
+            {
+                coins = value;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        //Make sure only one instance exists, singleton
+        if (GameManager.instance == null)
+        {
+            GameManager.instance = this;
+        }
+        else if (GameManager.instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
