9844d56cfe9e6363ce41e060aa46063b0a10d6b8
Eleanor Post (RIT Student) <eap9238@ad.rit.edu>
2019-03-25 17:49:12 -0400

    finished adding fishes, fully implemented rice and eating, fleshed out random walk system, prevented screen leav, added fullness states and visual cues, filled out death

---
+++ Assets/Rice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rice : MonoBehaviour
+{
+    public float foodVal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject fish in GameObject.FindGameObjectsWithTag("Fish"))
+        {
+            fish.SendMessage("addRice", gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position -= new Vector3(0.0f, 0.25f, 0.0f) * Time.deltaTime;
+    }
+
+    private void OnDestroy()
+    {
+        foreach (GameObject fish in GameObject.FindGameObjectsWithTag("Fish"))
+        {
+            fish.SendMessage("removeRice", gameObject);
+        }
+    }
+}
--- Assets/Scripts/FishManager.cs
+++ Assets/Scripts/FishManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FishManager : MonoBehaviour
 {
-    private enum FishState
+    public enum FishState
     {
         Full,
         Norm,
@@ -12,6 +12,8 @@ public class FishManager : MonoBehaviour
     }
 
     // Start is called before the first frame update
+
+    public float interval;
     public Sprite fishFull;
     public Sprite fishNorm;
     public Sprite fishSick;
@@ -20,29 +22,76 @@ public class FishManager : MonoBehaviour
     private float hunger;
     private FishState fishState;
 
+    private float x;
+    private float y;
+    private Camera camera;
+
     private SpriteRenderer image;
     private Rigidbody2D rigidbody2D;
+    private List<GameObject> riceList = new List<GameObject>();
+    private GameObject closestRice;
 
     void Start()
     {
-        hunger = 60;
+        hunger = 2.5f * interval;
         fishState = FishState.Full;
+
+        Camera camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        x = camera.ViewportToWorldPoint(new Vector3(1, 1)).x - 1.75f;
+        y = camera.ViewportToWorldPoint(new Vector3(1, 1)).y - 1.5f;
 
         image = gameObject.GetComponent<SpriteRenderer>();
         rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+
+        riceList.AddRange(GameObject.FindGameObjectsWithTag("Rice"));
     }
 
     // Update is called once per frame
     void Update()
     {
+        //avoid edges
+        avoidEdge();
+
         //depending on state and availability of food, seek food/idleswim?
-        if (true) //assuming no food
+        if (fishState != FishState.Full && riceList.Count > 0) //assuming food & hungry
         {
-            rigidbody2D.velocity += new Vector2(Random.Range(-0.25f, 0.25f), Random.Range(-0.25f, 0.25f));
+            if (closestRice == null)
+            {
+                assignClosestRice();
+            }
+            else
+            {
+                Vector2 direction = new Vector2(closestRice.transform.position.x - gameObject.transform.position.x, closestRice.transform.position.y - gameObject.transform.position.y).normalized;
+
+                rigidbody2D.AddForce(direction);
+
+                rigidbody2D.velocity += direction;
+            }
+
+            if (rigidbody2D.velocity.magnitude > 2.0f)
+            {
+                rigidbody2D.velocity = rigidbody2D.velocity.normalized * 2.0f;
+            }
+        }
+        else
+        {
+            rigidbody2D.AddForce(new Vector2(Random.Range(-2f, 2f), Random.Range(-2f, 2f)));
         }
 
         hunger -= Time.deltaTime;
         checkState();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Rice" && fishState != FishState.Full)
+        {
+            hunger += collision.gameObject.GetComponent<Rice>().foodVal;
+
+            rigidbody2D.velocity /= 5.0f;
+
+            Destroy(collision.gameObject);
+        }
     }
 
     public void checkState()
@@ -50,9 +99,9 @@ public class FishManager : MonoBehaviour
         switch (fishState)
         {
             case FishState.Full:
-                if (hunger < 60)
+                if (hunger < interval * 3.0f)
                 {
-                    if (hunger <= 40)
+                    if (hunger <= interval * 2.0f)
                     {
                         fishState = FishState.Norm;
                         image.sprite = fishNorm;
@@ -61,15 +110,12 @@ public class FishManager : MonoBehaviour
                 break;
 
             case FishState.Norm:
-                if (hunger > 40)
+                if (hunger > interval * 2.0f)
                 {
-                    if (hunger <= 40)
-                    {
-                        fishState = FishState.Full;
-                        image.sprite = fishFull;
-                    }
+                    fishState = FishState.Full;
+                    image.sprite = fishFull;
                 }
-                else if(hunger <= 20)
+                else if (hunger <= interval * 1.0f)
                 {
                     fishState = FishState.Sick;
                     image.sprite = fishSick;
@@ -77,9 +123,9 @@ public class FishManager : MonoBehaviour
                 break;
 
             case FishState.Sick:
-                if (hunger > 40)
+                if (hunger > interval * 2.0f)
                 {
-                    if (hunger <= 20)
+                    if (hunger <= interval * 1.0f)
                     {
                         fishState = FishState.Norm;
                         gameObject.GetComponent<SpriteRenderer>().sprite = fishNorm;
@@ -94,11 +140,68 @@ public class FishManager : MonoBehaviour
             default:
                 die();
                 break;
-
         }
     }
 
-    public void die()
+    public void addRice(GameObject rice)
+    {
+        if (!riceList.Contains(rice))
+        {
+            riceList.Add(rice);
+
+            assignClosestRice();
+        }
+    }
+
+    public void removeRice(GameObject rice)
+    {
+        riceList.Remove(rice);
+    }
+
+    private void avoidEdge()
+    {
+        //avoid y edge
+        if (gameObject.transform.position.y > y)
+        {
+            rigidbody2D.AddForce(new Vector2(0.0f, -1.0f));
+        }
+        else if (gameObject.transform.position.y < -y)
+        {
+            rigidbody2D.AddForce(new Vector2(0.0f, 1.0f));
+        }
+
+        //avoid x edge
+        if (gameObject.transform.position.x > x)
+        {
+            rigidbody2D.AddForce(new Vector2(-1.0f, 0.0f));
+        }
+        else if (gameObject.transform.position.x < -x)
+        {
+            rigidbody2D.AddForce(new Vector2(1.0f, 0.0f));
+        }
+
+        if (rigidbody2D.velocity.magnitude > 1.5f)
+        {
+            rigidbody2D.velocity = rigidbody2D.velocity.normalized * 1.5f;
+        }
+    }
+
+    private void assignClosestRice()
+    {
+        float mag = 100;
+
+        foreach (GameObject rice in riceList)
+        {
+            if (Vector3.Distance(gameObject.transform.position, rice.transform.position) < mag)
+            {
+                closestRice = rice;
+
+                mag = Vector3.Distance(gameObject.transform.position, rice.transform.position);
+            }
+        }
+    }
+
+    private void die()
     {
         Instantiate(deadFish, transform.position, Quaternion.identity);
 
