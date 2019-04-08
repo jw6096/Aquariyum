40b6151e4732a6bb6ddc8a125fde66c1919a0ed4
Eleanor Post (RIT Student) <eap9238@ad.rit.edu>
2019-04-08 10:55:54 -0400

    working on chopstick script

---
+++ Assets/Scripts/ChopStickManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopStickManager : MonoBehaviour
+{
+    private GameObject fish;
+    private GameObject sushi;
+    private GameObject prey;
+
+    private List<GameObject> preyFish;
+
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < fish.transform.childCount; i++)
+        {
+            preyFish.Add(fish.transform.GetChild(i).gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            if (sushi != null)
+            {
+                sushi.transform.parent = fish.transform;
+            }
+
+            //chopsticks float up, die when above screen. 
+        }
+        else
+        {
+            if (sushi != null)
+            {
+                //float upwards, destroy sushi when above screen
+            }
+
+            //seek nearest fish, grab and parent, set parented fish to sushi
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        health--;
+
+        if (sushi != null)
+        {
+            sushi.transform.parent = fish.transform;
+        }
+    }
+}
